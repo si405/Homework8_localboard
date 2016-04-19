4dcdad4a748b98dc0426ff4a50f08607beb42169
simon-ibh <simon@icebreakerhealth.com>
2016-04-18 19:53:00 -0700

    Testing the Instagram authentication

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+        // Prepare the Handlebars template
+
+        var source = $("#song-template").html();
+        var songTemplate = Handlebars.compile(source);
+
+        // Look for returned hash token from Spotify
+        // Regex to find only the access token from the URL
+
+        if (accessToken === '') {
+            var tokenMatches = window.location.hash.match(/access_token=(.*)&token_type=*/);
+            if (tokenMatches) {
+                accessToken = tokenMatches[1];
+                // Store the values in session storage that will be cleared when the browser closes
+                // Local storage would persist when the browser closes, which we don't want
+                window.sessionStorage.setItem("instagram_access_token", accessToken);
+                console.log(accessToken);
+            };
+        };
+
+//end of document
+});
